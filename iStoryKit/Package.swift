268842fc65b4428c366dc02001709c949c7baea9
Nikola Stojanovic <nikola.stojanovic@codecentric.de>
2022-03-19 00:31:26 +0100

    Implement keychain modules, add Alamofire, add Nuke

--- iStoryKit/Package.swift
+++ iStoryKit/Package.swift
@@ -6,23 +6,26 @@ import PackageDescription
 let package = Package(
     name: "iStoryKit",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "MyLibrary",
-            targets: ["MyLibrary"]),
+            name: "KeychainService",
+            targets: ["KeychainService"]
+        ),
+        .library(
+            name: "KeychainServiceAPI",
+            targets: ["KeychainServiceAPI"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(name: "Alamofire", url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.4.0")),
+        .package(name: "Nuke", url: "https://github.com/kean/Nuke.git", .upToNextMajor(from: "10.0.0"))
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "MyLibrary",
-            dependencies: []),
-        .testTarget(
-            name: "MyLibraryTests",
-            dependencies: ["MyLibrary"]),
+            name: "KeychainServiceAPI",
+            dependencies: []
+        ),
+        .target(
+            name: "KeychainService",
+            dependencies: ["KeychainServiceAPI"]
+        )
     ]
 )
---
+++ iStoryKit/Sources/KeychainServiceAPI/KeychainServiceAPI.swift
@@ -0,0 +1,22 @@
+
+
+public protocol KeychainServiceAPI {
+    @discardableResult func setAccessToken(_ token: String) -> Bool
+    func getAccessToken() -> String?
+    @discardableResult func setRefreshToken(_ token: String) -> Bool
+    func getRefreshToken() -> String?
+    @discardableResult func deleteAccessToken() -> Bool
+    @discardableResult func deleteRefreshToken() -> Bool
+    @discardableResult func clear() -> Bool
+}
+
+public protocol KeychainWrapperAPI {
+    func set(_ value: String, forKey key: String) -> Bool
+    func set(_ value: Data, forKey key: String) -> Bool
+    func set(_ value: Bool, forKey key: String) -> Bool
+    func get(_ key: String) -> String?
+    func get(_ key: String) -> Data?
+    func get(_ key: String) -> Bool?
+    func delete(_ key: String) -> Bool
+    func clear() -> Bool
+}
