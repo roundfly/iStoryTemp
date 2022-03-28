431cce827568f81fa02735063f7db2000e6b09cc
BratislavBaljak <bratislav.baljak@gmail.com>
2022-03-28 17:31:24 +0200

    IS-398-appdelegate-cleanup

--- iStory/AppDelegate.swift
+++ iStory/AppDelegate.swift
@@ -6,18 +6,12 @@
 //
 
 import UIKit
-import LoginWithAmazon
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        setupAmazon()
         
         return true
-    }
-    
-    private func setupAmazon() {
-        
     }
 }
 
