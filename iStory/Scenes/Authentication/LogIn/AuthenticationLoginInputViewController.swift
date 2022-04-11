6c599f66a67441b9143e25a622e94f885a489a69
Nikola Stojanovic <nikola.stojanovic@codecentric.de>
2022-04-11 20:10:06 +0200

    Setup login error label

--- iStory/Scenes/Authentication/LogIn/AuthenticationLoginInputViewController.swift
+++ iStory/Scenes/Authentication/LogIn/AuthenticationLoginInputViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 import StyleSheet
 
 final class AuthenticationLoginInputViewController: UIViewController {
+    // MARK: - Instance variables
 
     var forgotPasswordPublisher: AnyPublisher<Void, Never> {
         forgotPasswordSubject.eraseToAnyPublisher()
@@ -22,16 +23,22 @@ final class AuthenticationLoginInputViewController: UIViewController {
     private let createAccountSubject = PassthroughSubject<Void, Never>()
     private var authInputView: AuthenticationInputView!
     private let viewModel: AuthenticationInputViewModel
+    private let errorLabel = UILabel()
     private var cancellables: Set<AnyCancellable> = []
+
+    // MARK: - Initialization
 
     init(store: AuthenticationStore) {
         self.viewModel = AuthenticationInputViewModel(authenticationType: .login, store: store)
         super.init(nibName: nil, bundle: nil)
         viewModel.store.$state
             .dropFirst()
-            .sink { authState in
+            .sink { [weak self] authState in
                 if let error = authState.authFailure {
-                    print(error)
+                    self?.show(failureReason: error)
+                } else if let _ = authState.currentUser {
+                    // do stuff with user
+                    self?.hideFailureLabel()
                 }
             }
             .store(in: &cancellables)
@@ -40,6 +47,8 @@ final class AuthenticationLoginInputViewController: UIViewController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+
+    // MARK: - View controller lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -47,6 +56,8 @@ final class AuthenticationLoginInputViewController: UIViewController {
         setupSubviews()
         hideKeyboardWhenTappedAround()
     }
+
+    // MARK: - Subview setup
 
     private func setupSubviews() {
         setupInputView()
@@ -82,5 +93,32 @@ final class AuthenticationLoginInputViewController: UIViewController {
         forgotPasswordButton.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor).activate()
         forgotPasswordButton.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor).activate()
         forgotPasswordButton.heightAnchor.constraint(equalToConstant: 44.0).activate()
+    }
+
+    private func show(failureReason: String) {
+        errorLabel.text = failureReason
+        guard errorLabel.superview == nil else { return }
+        errorLabel.alpha = 0.0
+        errorLabel.font = .preferredFont(forTextStyle: .body)
+        errorLabel.numberOfLines = 0
+        errorLabel.textAlignment = .center
+        view.addManagedSubview(errorLabel)
+        errorLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).activate()
+        errorLabel.leadingAnchor.constraint(equalTo: view.readableContentGuide.leadingAnchor).activate()
+        errorLabel.trailingAnchor.constraint(equalTo: view.readableContentGuide.trailingAnchor).activate()
+        errorLabel.topAnchor.constraint(equalTo: authInputView.bottomAnchor, constant: 40).activate()
+        UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 0.5, delay: 0, options: .curveLinear, animations: {
+            self.errorLabel.alpha = 1.0
+        })
+    }
+
+    private func hideFailureLabel() {
+        UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 0.5, delay: 0, options: .curveLinear, animations: {
+            self.errorLabel.alpha = 0.0
+        }) { position in
+            if position == .end {
+                self.errorLabel.removeFromSuperview()
+            }
+        }
     }
 }
